f54e678ebf221244f3717bb8efba9609dde760bb
Alex Rutar <alex@rutar.org>
2023-08-13 21:17:24 +0100

    fix build script

--- pdf_data.py
+++ pdf_data.py
@@ -12,17 +12,9 @@ def generate_pdf_data(pdf_dir):
         }
 
 
-paper_page_counts = [
-    len(PdfReader(pdf).pages) for pdf in Path("static/papers").glob("*.pdf")
-]
-note_page_counts = [
-    len(PdfReader(pdf).pages) for pdf in Path("static/notes").glob("*.pdf")
-]
-
 if __name__ == "__main__":
     generated_data = {
         "papers": generate_pdf_data("static/papers"),
         "notes": generate_pdf_data("static/notes"),
     }
-    print(generated_data)
     Path("data/pdf_data.json").write_text(json.dumps(generated_data))
