dfe135727314dc0e26269b5bc5d04312977994ae
Alex Rutar <alex@rutar.org>
2023-08-17 14:04:04 +0100

    format with black

--- scripts/pdf_data.py
+++ scripts/pdf_data.py
@@ -6,10 +6,10 @@ import json
 def generate_pdf_data(pdf_dir):
     page_counts = [len(PdfReader(pdf).pages) for pdf in Path(pdf_dir).glob("*.pdf")]
     return {
-            "page_count": sum(page_counts),
-            "average_page_count": round(sum(page_counts) / len(page_counts), 1),
-            "num_files": len(page_counts),
-        }
+        "page_count": sum(page_counts),
+        "average_page_count": round(sum(page_counts) / len(page_counts), 1),
+        "num_files": len(page_counts),
+    }
 
 
 if __name__ == "__main__":
